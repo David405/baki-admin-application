a452c21818840e8422042509c335d1db8f4ac898
David Asamonye <davidasamonye@gmail.com>
2023-07-10 14:43:53 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const vault = "0x3Bd24da97a0d9a678d8746D4E7A26411ef3f364f";
-export const oracle = "0x9D41424580261574f8159793CdC76ca648a5EfE9";
+export const vault = "0x158855D3CA96D7a51601B2167FA1D322F8cC7c0e";
+export const oracle = "0x890e894077fB3860374E51A4c86c4358ec24C327";
